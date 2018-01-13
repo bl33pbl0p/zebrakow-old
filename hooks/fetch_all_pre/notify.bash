3963d9c25953abe3503ffb861a911e61ed4b7412
Jonathon Kowalski <worz@tuta.io>
2018-01-13 20:29:16 +0530

    Fix zebrakow-setup and activation
    With Type=notify, we shouldn't wait for the unit to activate
    and instead use --no-block, but if that appears to be desirable
    in the future, a separate option can be added.

--- hooks/fetch_all_pre/notify.bash
+++ hooks/fetch_all_pre/notify.bash
@@ -1,7 +1,7 @@
 if [[ ! -z "$INVOCATION_ID" ]]; then
     systemd-notify --booted
     if [[ echo $? == "0" ]]; then
-	systemd-notify --status="fetch_all_pre: Starting all downloads..."
+	systemd-notify --ready --status="fetch_all_pre: Starting all downloads..."
 	# systemd has a race where exiting too quickly may result in
 	# no attribution of the notifications or status to the unit
 	sleep 0.5
